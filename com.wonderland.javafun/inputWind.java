27b92969e42254bb4313cdc12886c548db9bc097
Garima <amgarima264@gmail.com>
2024-01-10 00:12:33 +0530

    new files added

---
+++ com.wonderland.javafun/inputWind.java
@@ -0,0 +1,14 @@
+package com.wonderland.javafun;
+
+import java.util.Scanner;
+
+class inputWind
+{
+    public static void main (String[] args)
+    {
+        // your code goes here
+        Scanner sc = new Scanner(System.in);
+        String var = sc.nextLine();
+        System.out.println(var);
+    }
+}
